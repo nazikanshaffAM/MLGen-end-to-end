6b088e6615c795ce8f311d2d549d11ca6d3fc9ae
nazikanshaffAM <anshaff.nzk@gmail.com>
2025-06-16 03:38:14 +0530

    Add: added the eda and some modelling process

--- src/exception.py
+++ src/exception.py
@@ -1,6 +1,7 @@
 # All the exceptions are handled in this file
 
 import sys 
+from src.logger import logging
 
 def error_message_detail(error, error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()
@@ -16,4 +17,4 @@ class CustomException(Exception):
         self.error_message = error_message_detail(error_message, error_detail = error_detail) 
     
     def __str__(self):
-        return self.error_message
+        return self.error_message    
